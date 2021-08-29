5e0b122f602017272d9cbc88eac18a4bbb69292f
dispet <liftesk.@gmail.com>
2021-08-30 02:16:43 +0300

    feat: add shop-fe

---
+++ src/constants/apiPaths.ts
@@ -0,0 +1,10 @@
+
+const API_PATHS = {
+  product: 'https://h0ajk56rl8.execute-api.eu-west-1.amazonaws.com/dev',
+  order: 'https://h0ajk56rl8.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://h0ajk56rl8.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://h0ajk56rl8.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://h0ajk56rl8.execute-api.eu-west-1.amazonaws.com/dev',
+};
+
+export default API_PATHS;
